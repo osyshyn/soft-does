fff2e606b6f87e59204270ca67ebb1734dc8f4dc
osyshyn <ceo@comebackagency.com>
2025-08-05 23:33:19 +0300

    Add meta

--- src/shared/ui/seo/index.tsx
+++ src/shared/ui/seo/index.tsx
@@ -11,6 +11,7 @@ export const SEO: React.FC<IProps> = ({ pathname, title, description }) => (
     <title key="title">{title}</title>
     <meta key="description" name="description" content={description} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="msvalidate.01" content="B318AD364B2B8B51BA2CD9973949B158" />
     <link rel="icon" href="/favicon.ico" />
     <link rel="canonical" href={`https://softdoes.com${pathname}`} />
   </Head>
